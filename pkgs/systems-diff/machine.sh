34df0dbd61a93099cbf75583d0b8d2bd3a428874
Jakub Sokołowski <jakub@status.im>
2026-09-18 14:18:59 +0200

    pkgs/systems-diff: use git worktree for master checkout
    Also improve variable names and avoid changing folders.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/systems-diff/machine.sh
+++ pkgs/systems-diff/machine.sh
@@ -21,16 +21,16 @@ function log_warning() { echo -e "\033[0;33m[WARNING]\033[0m $1" >&2; }
 function log_error()   { echo -e "\033[0;31m[ERROR]\033[0m   $1" >&2; }
 
 function dependencies_diff_for_machine() {
-  local master_config=$1 pr_config=$2 machine=$3
+  local master_config=$1 current_config=$2 machine=$3
   local summary_file="${SYSTEMS_DIFF_DIR}/${machine}.summary"
 
-  if [[ -z "${pr_config}" ]]; then
-    log_warning "Machine config missing in PR! No such config: ${pr_config}"
+  if [[ -z "${current_config}" ]]; then
+    log_warning "Machine config missing in PR! No such config: ${current_config}"
     return 0
   fi
   log_info "Generating dependencies diff for ${machine}..."
 
-  if ! deps_diff=$(nix store diff-closures "${master_config}" "${pr_config}" 2>&1); then
+  if ! deps_diff=$(nix store diff-closures "${master_config}" "${current_config}" 2>&1); then
     log_error "Failed to generate dependencies diff for ${machine}"
     # Show stderr from failed diff-closures command.
     echo "${deps_diff}"
@@ -51,19 +51,18 @@ function dependencies_diff_for_machine() {
 }
 
 function detect_systemd_services_updates_for_machine() {
-  local master_config=$1 pr_config=$2 machine=$3
+  local master_config=$1 current_config=$2 machine=$3
   local summary_file="${SYSTEMS_DIFF_DIR}/${machine}.summary"
 
-  master_systemd_dir="$master_config/etc/systemd/system/"
-  pr_systemd_dir="$pr_config/etc/systemd/system/"
+  master_systemd_dir="${master_config}/etc/systemd/system"
+  current_systemd_dir="${current_config}/etc/systemd/system"
 
-  if [[ ! -d "$master_systemd_dir" ]]; then
-    log_error "Master systemd directory does not exist: $master_systemd_dir"
+  if [[ ! -d "${master_systemd_dir}" ]]; then
+    log_error "Master systemd directory does not exist: ${master_systemd_dir}"
     return 1
   fi
-
-  if [[ ! -d "$pr_systemd_dir" ]]; then
-    log_error "PR systemd directory does not exist: $pr_systemd_dir"
+  if [[ ! -d "${current_systemd_dir}" ]]; then
+    log_error "PR systemd directory does not exist: ${current_systemd_dir}"
     return 1
   fi
 
@@ -71,32 +70,32 @@ function detect_systemd_services_updates_for_machine() {
   declare -a SERVICES_TO_START=()
   declare -a SERVICES_TO_STOP=()
 
-  log_info "Analyzing systemd services which will be started or restarted on $machine."
-  for service_file in "$pr_systemd_dir"/*.service; do
-    if [ -f "$service_file" ]; then
-      service_name=$(basename "$service_file")
-      master_service_file="$master_systemd_dir/$service_name"
+  log_info "Analyzing systemd services which will be started or restarted on ${machine}."
+  for service_file in "${current_systemd_dir}"/*.service; do
+    if [ -f "${service_file}" ]; then
+      service_name=$(basename "${service_file}")
+      master_service_file="${master_systemd_dir}/${service_name}"
       service_basename="${service_name%.service}"
 
-      if [ -f "$master_service_file" ]; then
-        if ! diff -q "$service_file" "$master_service_file" >/dev/null; then
-          SERVICES_TO_RESTART+=("$service_basename")
+      if [ -f "${master_service_file}" ]; then
+        if ! diff -q "${service_file}" "${master_service_file}" >/dev/null; then
+          SERVICES_TO_RESTART+=("${service_basename}")
         fi
       else
-        SERVICES_TO_START+=("$service_basename")
+        SERVICES_TO_START+=("${service_basename}")
       fi
     fi
   done
 
-  log_info "Analyzing systemd services which will be stopped on $machine."
-  for service_file in "$master_systemd_dir"/*.service; do
-    if [ -f "$service_file" ]; then
-      service_name=$(basename "$service_file")
-      pr_service_file="$pr_systemd_dir/$service_name"
+  log_info "Analyzing systemd services which will be stopped on ${machine}."
+  for service_file in "${master_systemd_dir}"/*.service; do
+    if [ -f "${service_file}" ]; then
+      service_name=$(basename "${service_file}")
+      current_service_file="${current_systemd_dir}/${service_name}"
       service_basename="${service_name%.service}"
 
-      if [ ! -f "$pr_service_file" ]; then
-        SERVICES_TO_STOP+=("$service_basename")
+      if [ ! -f "${current_service_file}" ]; then
+        SERVICES_TO_STOP+=("${service_basename}")
       fi
     fi
   done
@@ -110,39 +109,45 @@ function detect_systemd_services_updates_for_machine() {
 }
 
 function nix_derivation_diff_for_machine() {
-  local master_config=$1 pr_config=$2 machine=$3
+  local master_config=$1 current_config=$2 machine=$3
   local diff_file="${SYSTEMS_DIFF_DIR}/${machine}.diff"
 
-  if [[ -z "${pr_config}" ]]; then
-    log_warning "Machine config missing in PR! No such config: ${pr_config}"
+  if [[ -z "${current_config}" ]]; then
+    log_warning "Machine config missing in PR! No such config: ${current_config}"
     return 0
   fi
   log_info "Analyzing derivation differences for $machine."
-  nix-diff --color=always --word-oriented --context=6 "${master_config}" "${pr_config}" \
+  nix-diff --color=always --word-oriented --context=6 "${master_config}" "${current_config}" \
       > "${diff_file}"
 }
 
 function generate_machine_config() {
   local machine="${1}" commit="${2}" branch="${3}"
+  local dir="${PWD}"
   log_info "Building ${branch} (${commit:0:8}) configuration for: ${machine}"
-  git checkout -q "${commit}"
-  if [[ ! -d "machines/${machine}" ]]; then
+  if [[ $(git rev-parse HEAD) != "${commit}" ]]; then
+    git worktree add --quiet "./${commit}" "${commit}"
+    # shellcheck disable=SC2064
+    trap "git worktree remove '${commit}'" EXIT
+    dir=${PWD}/${commit}
+  fi
+  if [[ ! -d "${dir}/machines/${machine}" ]]; then
     log_warning "Skipping missing machine: ${machine}"
     return
   fi
   nix build --no-link --print-out-paths \
-    '.#nixosConfigurations."'"${machine}"'".config.system.build.toplevel'
+    "${dir}#nixosConfigurations.\"${machine}\".config.system.build.toplevel"
 }
 
 function generate_machine_diff() {
   local machine=$1
 
-  master_config=$(generate_machine_config "${machine}" "${MASTER_HEAD}" "master")
-  pr_config=$(generate_machine_config "${machine}" "${PR_HEAD}" "pr")
+  master_config=$(generate_machine_config "${machine}" "${MASTER_COMMIT}" "master")
+  current_config=$(generate_machine_config "${machine}" "${CURRENT_COMMIT}" "current")
 
-  detect_systemd_services_updates_for_machine "${master_config}" "${pr_config}" "${machine}"
-  dependencies_diff_for_machine "${master_config}" "${pr_config}" "${machine}"
-  nix_derivation_diff_for_machine "${master_config}" "${pr_config}" "${machine}"
+  detect_systemd_services_updates_for_machine "${master_config}" "${current_config}" "${machine}"
+  dependencies_diff_for_machine "${master_config}" "${current_config}" "${machine}"
+  nix_derivation_diff_for_machine "${master_config}" "${current_config}" "${machine}"
 
   log_success "Diff generation complete for ${machine}"
 }
@@ -155,11 +160,10 @@ function generate_machine_diff() {
 IS_GITHUB_ACTION=${GITHUB_ACTIONS:-false}
 SYSTEMS_DIFF_DIR=${SYSTEMS_DIFF_DIR:-"./systems-diff"}
 
-PR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-PR_HEAD=$(git rev-parse HEAD)
-MASTER_HEAD=$(git merge-base origin/master HEAD)
+CURRENT_COMMIT=$(git rev-parse HEAD)
+MASTER_COMMIT=$(git merge-base origin/master HEAD)
 
-trap 'git checkout -q "${PR_BRANCH}"' EXIT
+[[ "${CURRENT_COMMIT}" == "${MASTER_COMMIT}" ]] && { log_warning "Nothing to compare." >&2;     exit 0; }
 
 generate_machine_diff "${1}"
 if [[ "${IS_GITHUB_ACTION}" == "false" ]]; then
